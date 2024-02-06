75ddde75d4aa5b226bdc4e13ec87823bffa72c4a
Han-D-Peter <secret_wings@naver.com>
2024-02-06 14:47:23 +0900

    chore

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  // CRD 구현(칼카피 필요 없음)
+  // 1. 투두 생성 기능
+  // 2. 투두 리스팅
+  // 3. 체크박스 업데이트 기능 (상태 사용)
+  // 4. 삭제버튼으로 삭제 기능
+  return <div className="App">hello</div>;
+}
